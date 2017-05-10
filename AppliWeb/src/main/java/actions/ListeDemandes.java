9cd034de68b85316dcf3c23862fb9f68a3dfe3a5
anthony.loup@etu.univ-lyon1.fr <anthony.loup01@gmail.com>
2017-05-10 10:54:31 +0200

    ListeDemandesAction

---
+++ AppliWeb/src/main/java/actions/ListeDemandes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actions;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Anthony
+ */
+public class ListeDemandes extends Action{
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        //TODO
+        return null;
+    }
+    
+}
