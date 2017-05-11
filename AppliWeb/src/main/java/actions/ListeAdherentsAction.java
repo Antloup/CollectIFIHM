a43a11506c6e59f82aa612d54bd82c60fd5a5f95
anthony.loup@etu.univ-lyon1.fr <anthony.loup01@gmail.com>
2017-05-11 08:45:00 +0200

    Liste

---
+++ AppliWeb/src/main/java/actions/ListeAdherentsAction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actions;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Anthony
+ */
+public class ListeAdherentsAction extends Action{ // Peut etre pas utile
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
